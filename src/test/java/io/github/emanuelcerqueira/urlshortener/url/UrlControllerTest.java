73c583b628456d90e6f56d2180b907ce7e24dc41
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2020-10-08 16:43:03 -0300

    Adding print message and more tests

--- src/test/java/io/github/emanuelcerqueira/urlshortener/url/UrlControllerTest.java
+++ src/test/java/io/github/emanuelcerqueira/urlshortener/url/UrlControllerTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -36,6 +38,7 @@ class UrlControllerTest {
         mockMvc.perform(post("/url/shorten")
                 .contentType("application/json")
                 .content(body))
+                .andDo(print())
                 //then
                 .andExpect(status().isOk());
 
@@ -51,6 +54,7 @@ class UrlControllerTest {
         mockMvc.perform(post("/url/shorten")
                 .contentType("application/json")
                 .content(body))
+                .andDo(print())
                 //then
                 .andExpect(content().string(containsString("\"slug\":")));
     }
@@ -66,9 +70,48 @@ class UrlControllerTest {
         mockMvc.perform(post("/url/shorten")
                 .contentType("application/json")
                 .content(body))
+                .andDo(print())
                 //then
                 .andExpect(status().isUnprocessableEntity());
 
+    }
+
+    @Test
+    void  redirectToUrlBySlug_whenSlugIsValid_ThenReturnsStatus302() throws Exception {
+
+        //this value need to be in the database
+        //when
+        String validSlug = "8sVdO6";
+
+        mockMvc.perform(get("/"+validSlug))
+                .andDo(print())
+                //then
+                .andExpect(status().isFound());
+    }
+
+
+    @Test
+    void redirectToUrlBySlug_whenSlugIsValid_ThenReturnsHeaderContainingLocation() throws Exception {
+
+        //this value need to be in the database
+        //when
+        String validSlug = "8sVdO6";
+
+        mockMvc.perform(get("/"+validSlug))
+                .andDo(print())
+                //then
+                .andExpect(header().exists(HttpHeaders.LOCATION));
+    }
+    @Test
+    void  redirectToUrlBySlug_whenSlugIsInvalid_ThenReturnsStatus404() throws Exception {
+
+        //when
+        String invalidSlug = "this_slug_is_invalid";
+
+        mockMvc.perform(get("/"+invalidSlug))
+                .andDo(print())
+                //then
+                .andExpect(status().isNotFound());
     }
 
     private UrlRequest validInput() {
